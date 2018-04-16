8e6abca31d74cd9250c48ea5fcfa03c2928c96bf
David Herges <dherges@users.noreply.github.com>
2018-04-16 11:13:03 +0200

    fix: migrate tests

--- apps/one-app/src/app/app.component.spec.ts
+++ apps/one-app/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TwoLibModule } from '@nx-packaged/two-lib';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ describe('AppComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
+        imports: [TwoLibModule],
         declarations: [AppComponent]
       }).compileComponents();
     })
